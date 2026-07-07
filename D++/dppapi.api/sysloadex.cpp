5e300329906fe7c70a9719b9505fbb38e03060e4
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-07 07:04:23 +0200

    Create sysloadex.cpp

---
+++ D++/dppapi.api/sysloadex.cpp
@@ -0,0 +1,23 @@
+enum pointer
+{
+    IntPointers,
+    CharPointers,
+    DoublePointers,
+    FloatPointers,
+    EnumPointers
+};
+
+void pointbuf()
+{
+    void       * buffer;
+    pointer::IntPointers;
+};
+
+#include <windows.h>
+
+void reload
+{
+    // resimulate sysex
+    // getdata
+    return 0;
+}
