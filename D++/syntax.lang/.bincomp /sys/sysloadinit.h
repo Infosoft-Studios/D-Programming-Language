662c705c51d2c8a83db98643efb19e76cad6ab52
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-10 16:09:35 +0200

    Create sysloadinit.h for system load initialization
    Add system load initialization header with structure definition and main function.

---
+++ D++/syntax.lang/.bincomp /sys/sysloadinit.h
@@ -0,0 +1,16 @@
+#ifndef LOAD_SYSTEM_INIT_H;
+#define LOAD_SYSTEM_INIT_H;
+
+typedef struct loadsys
+{
+    _int64 = 99;
+    void* buffer();
+};
+
+#include <windows.h>
+
+int main()
+{
+    WINCLASS wc;
+    wc.loadinit                         = LoadSys(nullptr, "/$lnload$/");
+}
