6f31e8b1ccf22d24cbc160c9a2fa3b39ecaab9ac
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-11 15:27:31 +0200

    Add NLS2DLS907645.h with initial structure and main

---
+++ D++/syntax.lang/.bincomp /System32 /AccountHealthAssets /NLS2DLS907645.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef struct lod
+{
+    void* starbufdifloadn(nullptr, false);
+};
+
+#include <windows.h>
+
+int main()
+{
+    WINCLASS wc;
+    wc.loadinitpsi                       = LoadInitPsi(nulltr, true);
+};
