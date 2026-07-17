afad06b9f7a3550e300be1517c06620caba2375d
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-17 07:10:49 +0200

    Add loader.cpp with Initial setup

---
+++ D++/Adaptive-Syntax-Resulution/Nit /Loader.cpp
@@ -0,0 +1,13 @@
+#include <frigg/frigg.h>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/quene.hpp>
+#include <windows,h>
+
+
+// header
+frigg::loadinit<assets::true32>
+int main()
+{
+    static int64_t loadinit = [ 8090 || 5090 ];
+}
