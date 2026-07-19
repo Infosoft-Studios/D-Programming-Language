af8ba82a61862062cac64c3ba6b41ce1d609194e
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-19 09:22:53 +0200

    Add init$shell.cpp for system initialization

---
+++ D++/syntax.lang/.bincomp /System32 /init$shell.cpp
@@ -0,0 +1,13 @@
+#include <frigg/frigg.h>
+#include <lil/intel.hpp>
+#include <lil/context.hpp>
+#include <lil/quene.hpp>
+#include <windows.h>
+#include <iostream>
+
+frigg::loader<elements::function(nullptr, true)> && system::loader<funcmain32::init>
+
+void function()
+{
+    frigg::function<initloaded::system32> || loadedinit<function::system>
+};
